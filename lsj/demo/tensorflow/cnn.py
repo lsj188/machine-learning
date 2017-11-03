2e3753f201be5de10b83870265e19389e9298f8f
luoshijun <luoshijun58@163.com>
2017-11-03 16:00:54 +0800

    add cust img

--- lsj/demo/tensorflow/cnn.py
+++ lsj/demo/tensorflow/cnn.py
@@ -5,23 +5,17 @@
 ##*************************************************************************************************************
 ## **  文件名称：
 ## **  功能描述：  CNN卷积神经网络
+##
 ## **
-## **
-## **  输入：
-## **  输出：
-## **
-## **
-## **  创建者：骆仕军
-## **  创建日期：2017-10-09
-## **  修改日期：
-## **  修改日志：
+
 import datetime
 
 import tensorflow as tf
+from PIL import Image,ImageFilter
 from tensorflow.examples.tutorials.mnist import input_data
 
 # 载入数据集
-mnist = input_data.read_data_sets("../examples/tutorials/mnist/data", one_hot=True)
+mnist = input_data.read_data_sets("../../../test_data/img_data", one_hot=True)
 
 # 每个批次的大小
 batch_size = 100
@@ -29,7 +23,48 @@ batch_size = 100
 n_batch = mnist.train.num_examples // batch_size
 
 #图的保存目录
-graph_path="graphs/"
+graph_path="./graphs/"
+
+#模型参数据保存目录
+model_path="../../../model/img_cnn/data"
+
+
+def imageprepare(argv): # 该函数读一张图片，处理后返回一个数组，进到网络中预测
+    """
+    This function returns the pixel values.
+    The imput is a png file location.
+    """
+    im = Image.open(argv).convert('L')
+    width = float(im.size[0])
+    height = float(im.size[1])
+    newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
+
+    if width > height:  # check which dimension is bigger
+        # Width is bigger. Width becomes 20 pixels.
+        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
+        if nheight == 0:  # rare case but minimum is 1 pixel
+            nheight = 1
+            # resize and sharpen
+        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wtop = int(round(((28 - nheight) / 2), 0))  # caculate horizontal pozition
+        newImage.paste(img, (4, wtop))  # paste resized image on white canvas
+    else:
+        # Height is bigger. Heigth becomes 20 pixels.
+        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
+        if (nwidth == 0):  # rare case but minimum is 1 pixel
+            nwidth = 1
+            # resize and sharpen
+        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
+        newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
+
+    # newImage.save("sample.png")
+
+    tv = list(newImage.getdata())  # get pixel values
+
+    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
+    return tva
 
 #参数概要
 def variable_summaries(var):
@@ -138,36 +173,39 @@ with tf.name_scope("fc2"):
     with tf.name_scope("fc2_wx_plus_b"):
         fc2_wx_plus_b=tf.matmul(h_fc1_drop,fc2_w)+fc2_b
     with tf.name_scope("softmax"):
-        prediction=tf.nn.softmax(fc2_wx_plus_b)
+        fc2_y_conv=tf.nn.softmax(fc2_wx_plus_b)
 
 with tf.name_scope("cross_entropy"):
-    cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction),name="cross_entropy")
+    cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=fc2_y_conv),name="cross_entropy")
     tf.summary.scalar("cross_entropy",cross_entropy)
 
 with tf.name_scope("train"):
-    train_step=tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    train_step=tf.train.GradientDescentOptimizer(1e-4).minimize(cross_entropy)
 
 # 求准确率
 with tf.name_scope('accuracy'):
     with tf.name_scope('correct_prediction'):
         # 结果存放在一个布尔列表中
-        correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))  # argmax返回一维张量中最大的值所在的位置
+        correct_prediction = tf.equal(tf.argmax(fc2_y_conv, 1), tf.argmax(y, 1))  # argmax返回一维张量中最大的值所在的位置
     with tf.name_scope('accuracy'):
         # 求准确率
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         tf.summary.scalar('accuracy', accuracy)
 
+#定义saver
+saver = tf.train.Saver()
 # 合并所有的summary
 merged = tf.summary.merge_all()
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    # saver.restore(sess, model_path)  # restore参数
     train_writer = tf.summary.FileWriter(graph_path+'/train', sess.graph)
     test_writer = tf.summary.FileWriter(graph_path+'/test', sess.graph)
     for i in range(1001):
         # 训练模型
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 0.7})
+        sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1})
         # 记录训练集计算的参数
         summary = sess.run(merged, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.0})
         train_writer.add_summary(summary, i)
@@ -175,11 +213,31 @@ with tf.Session() as sess:
         batch_xs, batch_ys = mnist.test.next_batch(batch_size)
         summary = sess.run(merged, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.0})
         test_writer.add_summary(summary, i)
-
         if i % 100 == 0:
             test_acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels, keep_prob: 1.0})
             train_acc = sess.run(accuracy, feed_dict={x: mnist.train.images[:10000], y: mnist.train.labels[:10000],
                                                       keep_prob: 1.0})
             print("Iter " + str(i) + ", Testing Accuracy= " + str(test_acc) + ", Training Accuracy= " + str(train_acc))
+    saver.save(sess,model_path)
+    print("***************")
 
+#
+# with tf.Session() as sess:
+#     # 定义saver
+#     saver = tf.train.Saver()
+#     sess.run(tf.global_variables_initializer())
+#
+#     saver.restore(sess,model_path)  # restore参数
+#
+#     array = imageprepare('../../../test_data/img_data/my_img/6.png')  # 读一张包含数字的图片
+#     prediction = tf.argmax(fc2_y_conv, 1)  # 预测
+#
+#     prediction = prediction.eval(feed_dict={x: [array], keep_prob: 1.0}, session=sess)
+#     print('The digits in this image is:%d' % prediction[0])
+
+if __name__ == "__main__":
+    start = datetime.datetime.now()
+
+    end = datetime.datetime.now()
+    print('[' + end.now().strftime('%Y-%m-%d %H:%M:%S') + ']******模型运行时长为:' + str((end - start).seconds) + '秒 *********')
 
